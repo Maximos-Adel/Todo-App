bc9445e4ec2e6e0f1e82a57dcc7cd33e25336f2f
Maximos <72452730+Maximos94-ai@users.noreply.github.com>
2021-01-25 00:40:59 +0200

    persist

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,18 @@
 import { combineReducers } from "redux";
 import todos from "./todos";
 import visibilityFilter from "./visibilityFilter";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["todos"],
+};
+
+const rootReducer = combineReducers({
   todos,
   visibilityFilter,
 });
+
+export default persistReducer(persistConfig, rootReducer);
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+import { persistStore } from "redux-persist";
+import rootReducer from "./reducers";
+
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const persistor = persistStore(store);
